bea680a6ba19c35137065658d276b99b120c44f6
B-Boss <34366868+B-Boss@users.noreply.github.com>
2020-04-07 05:59:17 +0800

    V1.0.1

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ App({
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              // this.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
@@ -53,7 +53,8 @@ App({
   },
  
   globalData: {
-    url:"http://127.0.0.1:8080/",
+    // url:"http://59.110.22.158:8080/",
+    url:"http://192.168.0.104:8080/",
     userInfo: null,
     userId:'null',
     header:{'cookie':''},
